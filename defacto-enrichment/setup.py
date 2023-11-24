6614042ba1c0bc49d7fe98da8142900d71e1955e
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-11-24 17:18:15 +0100

    remove multithreading from crowdtangle api

--- defacto-enrichment/setup.py
+++ defacto-enrichment/setup.py
@@ -10,7 +10,7 @@ setup(
     python_requires=">=3.11",
     packages=find_packages(exclude=["schemas"]),
     install_requires=[
-        "minall @ git+https://github.com/medialab/minall.git@v0.0.11",
+        "minall @ git+https://github.com/medialab/minall.git@v0.0.12",
         "requests==2.31.0",
     ],
     entry_points={
