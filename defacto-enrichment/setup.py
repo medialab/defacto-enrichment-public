2348aa465ce4cda1cb7d1b37e77e250615f7ff56
Kelly Christensen <63346582+kat-kel@users.noreply.github.com>
2023-11-27 19:11:43 +0100

    install latest minall release

--- defacto-enrichment/setup.py
+++ defacto-enrichment/setup.py
@@ -10,7 +10,7 @@ setup(
     python_requires=">=3.11",
     packages=find_packages(exclude=["schemas"]),
     install_requires=[
-        "minall @ git+https://github.com/medialab/minall.git@v0.0.12",
+        "minall @ git+https://github.com/medialab/minall.git@v0.0.13",
         "requests==2.31.0",
     ],
     entry_points={
