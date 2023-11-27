29ed640a55a70359f16872470aed0384dfaedd46
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-11-27 17:15:01 +0100

    add command

--- compile-rss/src/cli.py
+++ compile-rss/src/cli.py
@@ -1,4 +1,6 @@
 import json
+import shutil
+from datetime import datetime
 from pathlib import Path
 
 import click
@@ -36,6 +38,27 @@ def get_history(output, repo, relative_file_name):
         outfile = outdir.joinpath(output_stem)
         with open(outfile, "w") as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+@cli.command("add")
+@click.option(
+    "--output",
+    type=click.Path(dir_okay=True, file_okay=False),
+    help="Path to the directory in which to write all the target files' committed versions.",
+)
+@click.argument("file", type=click.Path(dir_okay=False, file_okay=True))
+def add_file_to_history(output, file):
+    outdir = Path(output)
+    outdir.mkdir(exist_ok=True, parents=True)
+    fp = Path(file)
+    new_file_name = (
+        str(datetime.fromtimestamp(fp.stat().st_mtime).date())
+        + "_"
+        + fp.stem
+        + "".join(fp.suffixes)
+    )
+    outfile = outdir.joinpath(new_file_name)
+    shutil.copy(src=file, dst=str(outfile))
 
 
 @cli.command("flatten")
