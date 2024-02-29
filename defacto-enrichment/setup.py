8a4f876f37559ab44421c3cd64a96f0874f767a8
Kelly Christensen <kelly.christensen@sciencespo.fr>
2024-02-29 12:48:09 +0100

    upgrade minall

--- defacto-enrichment/setup.py
+++ defacto-enrichment/setup.py
@@ -14,7 +14,7 @@ setup(
     python_requires=">=3.11",
     packages=find_packages(exclude=["schemas"]),
     install_requires=[
-        "minall @ git+https://github.com/medialab/minall.git@v0.2.1",
+        "minall @ git+https://github.com/medialab/minall.git@v0.2.2",
         "requests==2.31.0",
     ],
     entry_points={
